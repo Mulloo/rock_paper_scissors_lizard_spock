679ab61b6a2fe3b248c4d6d43e863abb97965ba7
Mulloo <mulloolyd.1998@gmail.com>
2024-01-07 12:26:24 +0000

    add: fetch for json file & hide class function

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,11 +3,22 @@ let playerScore = 0;
 let cpuScore = 0;
 let rounds = 0;
 
-const winData = fetch('./json/data.json');
+document.addEventListener('DOMContentLoaded', function () {
+    fetchWinData();
+});
 
+async function fetchWinData() {
+    const res = await fetch('https://raw.githubusercontent.com/Mulloo/rock_paper_scissors_lizard_spock/main/assets/js/json/data.json');
+    winData = await res.json();
+    console.log(winData);
 
-function playGame(choice) {
-
+    console.log(winData.rock.wins);
 }
 
-console.log(winData);
+function addHideClass() {
+    document.getElementsByClassName('playerChoiceDiv').classlist.add('hide');
+}
+
+function playerChoice() {
+
+}
