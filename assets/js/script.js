afcb5ac46bf38607b64da4ffb6c42740ed6f9737
Mulloo <mulloolyd.1998@gmail.com>
2024-01-09 20:12:44 +0000

    fix: fav icon dicrectory corrected

--- assets/js/script.js
+++ assets/js/script.js
@@ -2,23 +2,29 @@
 let playerScore = 0;
 let cpuScore = 0;
 let rounds = 0;
+let levelDifficulty;
 const playerChoiceContainer = document.getElementsByClassName('player_choice_div');
 //! add target elements to id's here 
 
-
-/* waits for the DOM to load then fetches json data */
+/**
+ * waits for the DOM to load then fetches json data
+ */
 document.addEventListener('DOMContentLoaded', function () {
     fetchWinData();
     setLevelBtn();
+    addHideClass();
 });
 
 /* adds event listener to difficulty buttons and passes the user choice to the playGame function */
 const buttons = document.getElementsByTagName('button');
 
+/**
+ * sets the buttons for difficulty eventlistener and sends the input to the 
+ * playGame function
+ */
 function setLevelBtn() {
     for (let button of buttons) {
         button.addEventListener('click', function () {
-            console.log(this.getAttribute('game_type'));
             playGame(this.getAttribute('game_type'));
         })
     }
@@ -43,15 +49,48 @@ function addHideClass() {
     playerChoiceContainer.classlist.add('hide');
 }
 
+function showHiddenClass() {
+    playerChoiceContainer.classlist.remove('hide');
+}
+
+/**
+ * !
+ */
 function playerChoice() {
 
 }
 
-function playGame(levelDifficulty) {
-
-    addHideClass();
+/**
+ * grabs the level difficulty from playGame and uses the string passes to 
+ * decide what level of random to use.
+ */
+function computerChoice() {
+    if (levelDifficulty === 'easy') {
+        let numRandom = Math.floor(Math.random() * 3 + 1);
+    } else if (levelDifficulty === 'medium') {
+        let numRandom = Math.floor(Math.random() * 3 + 1);
+    } else if (levelDifficulty === 'hard') {
+        let numRandom = Math.floor(Math.random() * 3 + 1);
+    } else {
+        alert(`Unknown difficulty ${levelDifficulty}`)
+        throw `Unknown difficulty ${levelDifficulty}. Aborting!`;
+    }
+    determineWinner(numRandom);
+    console.log(numRandom);
+    console.log(levelDifficulty);
 }
 
+
+/**
+ * 
+ */
+function playGame(levelDifficulty) {
+    computerChoice(levelDifficulty);
+    showHiddenClass();
+}
+/**
+ * !
+ */
 function determineWinner() {
 
 }
