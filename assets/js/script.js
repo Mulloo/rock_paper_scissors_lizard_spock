0c58508433f516c5b718c3311661ddcd9cbc61ad
Mulloo <mulloolyd.1998@gmail.com>
2024-01-13 15:46:39 +0000

    restructur

--- assets/js/script.js
+++ assets/js/script.js
@@ -17,10 +17,25 @@ const btnChoice = document.getElementsByClassName('btn-choice');
  */
 document.addEventListener('DOMContentLoaded', function () {
     fetchWinData();
-    setLevelBtn();
+    for (let button of btnChoice) {
+        button.addEventListener('click', function (event) {
+            mainGameLogic(event);
+        })
+    }
     addHideClass();
 });
 
+/**
+ * !
+ */
+function mainGameLogic(event) {
+    const playerChoiceMade = event.target.getAttribute('data-choice');
+    console.log(`Choice Button clicked. Player Choose: ${playerChoiceMade}`);
+    numRandom = Math.floor(Math.random() * 5);
+    let computerChoiceMade = computerChoice(numRandom);
+    determineWinner(playerChoiceMade, computerChoiceMade);
+
+}
 /**
  * fetches the data form the winData.json file and stores it in the variable winData
  */
@@ -34,23 +49,6 @@ async function fetchWinData() {
     }
 }
 
-/**
- * grabs the level difficulty from playGame and uses the string passes to 
- * decide what level of random to use.
- */
-// function computerDifficulty(levelDifficulty) {
-//     const numRandomRange = 4;
-//     if (levelDifficulty === 'easy') {
-//         numRandom = Math.floor(Math.random() * (numRandomRange / 2));
-//     } else if (levelDifficulty === 'medium') {
-//         numRandom = Math.floor(Math.random() * (numRandomRange));
-//     } else if (levelDifficulty === 'hard') {
-//         numRandom = Math.floor(Math.random() * numRandomRange + 1);
-//     } else {
-//         alert(`Unknown difficulty ${levelDifficulty}`);
-//     }
-// }
-
 function computerChoice(numRandom) {
     const choices = Object.keys(winData);
     // const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
@@ -60,21 +58,7 @@ function computerChoice(numRandom) {
     return computerChoiceMade;
 }
 
-/**
- * sets the buttons for difficulty eventlistener and sends the input to the 
- * playGame function
- */
-function setLevelBtn() {
-    for (let button of btnChoice) {
-        button.addEventListener('click', function () {
-            const playerChoiceMade = this.getAttribute('data-choice');
-            console.log(`Choice Button clicked. Player Choose: ${playerChoiceMade}`);
-            numRandom = Math.floor(Math.random() * 5);
-            let computerChoiceMade = computerChoice(numRandom);
-            determineWinner(playerChoiceMade, computerChoiceMade);
-        })
-    }
-}
+
 
 /**
  *  adds the hide class to an element with classnames 
@@ -96,7 +80,6 @@ function determineWinner(playerChoiceMade, computerChoiceMade) {
         if (wins.includes(computerChoiceMade)) {
             result = `Player wins! ${playerChoiceMade} beats ${computerChoiceMade}`;
             console.log(result);
-
         } else {
             result = `Computer wins! ${computerChoiceMade} beats ${playerChoiceMade}`;
             console.log(result);
