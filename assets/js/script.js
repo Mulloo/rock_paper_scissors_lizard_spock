b29faabd9f1c15572e1b6b61af49ec0c36aa4a1e
Mulloo <mulloolyd.1998@gmail.com>
2024-01-13 15:34:06 +0000

    fix: scoping issue| difficulty deprecated

--- assets/js/script.js
+++ assets/js/script.js
@@ -4,11 +4,11 @@ let cpuScore = 0;
 let rounds = 0;
 let winData;
 let levelDifficulty;
+let numRandom;
 
 // target player choice div
 const playerChoiceContainer = document.getElementById("player_choice_div");
-// target difficulty buttons easy medium hard
-const btnDifficulty = document.getElementsByClassName('btn-difficulty');
+
 // target choice buttons ie. rock paper.etc
 const btnChoice = document.getElementsByClassName('btn-choice');
 
@@ -38,26 +38,25 @@ async function fetchWinData() {
  * grabs the level difficulty from playGame and uses the string passes to 
  * decide what level of random to use.
  */
-function computerDifficulty(levelDifficulty) {
-    let numRandom = Math.floor(Math.random() * 5); // initialize numRandom with a random value between 0 and 4
-    const numRandomRange = 4;
-    if (levelDifficulty === 'easy') {
-        numRandom = Math.floor(Math.random() * (numRandomRange / 2));
-    } else if (levelDifficulty === 'medium') {
-        numRandom = Math.floor(Math.random() * (numRandomRange));
-    } else if (levelDifficulty === 'hard') {
-        numRandom = Math.floor(Math.random() * numRandomRange + 1);
-    } else {
-        alert(`Unknown difficulty ${levelDifficulty}`);
-    }
-    computerChoice(numRandom);
-}
+// function computerDifficulty(levelDifficulty) {
+//     const numRandomRange = 4;
+//     if (levelDifficulty === 'easy') {
+//         numRandom = Math.floor(Math.random() * (numRandomRange / 2));
+//     } else if (levelDifficulty === 'medium') {
+//         numRandom = Math.floor(Math.random() * (numRandomRange));
+//     } else if (levelDifficulty === 'hard') {
+//         numRandom = Math.floor(Math.random() * numRandomRange + 1);
+//     } else {
+//         alert(`Unknown difficulty ${levelDifficulty}`);
+//     }
+// }
 
 function computerChoice(numRandom) {
     const choices = Object.keys(winData);
     // const choices = ['rock', 'paper', 'scissors', 'lizard', 'spock'];
     const computerChoiceMade = choices[numRandom];
     console.log(`Computer choose: ${computerChoiceMade}`);
+    console.log(numRandom);
     return computerChoiceMade;
 }
 
@@ -66,25 +65,14 @@ function computerChoice(numRandom) {
  * playGame function
  */
 function setLevelBtn() {
-    let computerChoiceMade;
-    for (let button of btnDifficulty) {
-        button.addEventListener('click', function () {
-            const difficulty = this.getAttribute("data-type");
-            console.log(`Button clicked. Difficulty: ${this.getAttribute("data-type")}`);
-            levelDifficulty = difficulty;
-            // playGame(this.getAttribute("data-type"));
-            // playerChoice(this.getAttribute("data-choice"));
-            computerDifficulty(this.getAttribute("data-type"));
-        });
-    }
     for (let button of btnChoice) {
         button.addEventListener('click', function () {
             const playerChoiceMade = this.getAttribute('data-choice');
             console.log(`Choice Button clicked. Player Choose: ${playerChoiceMade}`);
-            let computerChoiceMade;
+            numRandom = Math.floor(Math.random() * 5);
+            let computerChoiceMade = computerChoice(numRandom);
             determineWinner(playerChoiceMade, computerChoiceMade);
         })
-
     }
 }
 
@@ -100,21 +88,18 @@ function showHiddenClass() {
 }
 
 function determineWinner(playerChoiceMade, computerChoiceMade) {
-    while (computerChoiceMade = undefined) {
-        if (playerChoiceMade === computerChoiceMade) {
-            let result = 'it\'s a tie'
+    let result;
+    if (playerChoiceMade === computerChoiceMade) {
+        result = 'it\'s a tie'
+    } else {
+        const wins = winData[playerChoiceMade].wins;
+        if (wins.includes(computerChoiceMade)) {
+            result = `Player wins! ${playerChoiceMade} beats ${computerChoiceMade}`;
+            console.log(result);
+
         } else {
-            const wins = winData[playerChoiceMade].wins;
-            if (wins.includes(computerChoiceMade)) {
-                result = `Player wins! ${playerChoiceMade} beats ${computerChoiceMade}`;
-                console.log(result);
-            } else {
-                result = `Computer wins! ${computerChoiceMade} beats ${playerChoiceMade}`;
-                console.log(result);
-            }
+            result = `Computer wins! ${computerChoiceMade} beats ${playerChoiceMade}`;
+            console.log(result);
         }
-        return (result);
-        determineWinner(playerChoiceMade, computerChoiceMade);
-        break;
     }
 }
